21a78bc30395f4a1a2faf27e7687f014d0669140
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-03-28 18:25:30 -0400

    setup, commented, did a video walkthrough, hopefully its nice

---
+++ client/src-sw.js
@@ -0,0 +1,44 @@
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+// registers the files in the manifest for caching and routing
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Set up page cache
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// Set up asset cache
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+//this file deals with all the back end/offline functionality
+//warmstrategy cache lets us cache things proactively so we can utilizie it offline, whilst the registerroute functions let us cache the routes
